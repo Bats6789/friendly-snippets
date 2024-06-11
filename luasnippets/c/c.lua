eb30a5a8bb5e2da1758fd465b91f2857faf23709
Blake Wingard <bats23456789@gmail.com>
2024-06-11 10:47:36 -0500

    Added more snippets to C.
    Added if, switch, and some more macros. Currently working on a
    auto-documenting function snippet.

--- luasnippets/c/c.lua
+++ luasnippets/c/c.lua
@@ -134,19 +134,321 @@ return {
 	}),
 
 	s({
-		trig = '#if',
-		name = '#if macro',
-		desc = '#if snippet'
-	}, {
+			trig = '#if',
+			name = '#if macro',
+			desc = '#if snippet'
+		},
 		fmt([[
-		#if {cond}
-		{statement}
-		#endif /* if {cond} */
+		#{} {}
+		{}
+		#endif /* {} {} */
 		]], {
-			cond = i(1, '0'),
-			statement = i(0, '/* TODO */')
-		}, {
-			repeat_duplicate = true
+			c(1, {
+				t('if'),
+				t('ifdef'),
+				t('ifndef')
+			}),
+			i(2, '0'),
+			i(0, '/* TODO */'),
+			rep(1),
+			rep(2)
 		})
-	})
+	),
+
+	s({
+			trig = '#once',
+			name = 'Include once',
+			desc = 'Header guard'
+		},
+		fmt([[
+			#ifndef __{header_name}_H__
+			#define __{header_name}_H__
+			{start}
+
+			#endif /* end of header guard: __{header_name}_H__ */
+			]], {
+			header_name = f(function(_, _, _)
+				return string.upper(vim.fn.expand('%:t:r'))
+			end, {}, {}),
+			start = i(0),
+		}, {
+			repeate_duplicate = true
+		})
+	),
+
+	s({
+			trig = '#nocpp',
+			name = 'extern C',
+			desc = 'Disable C++ name mangling in C headers'
+		},
+		fmta([[
+		#ifdef __cplusplus
+		extern "C" {
+		#endif
+		<>
+
+		#ifdef __cplusplus
+		} /* extern "C" */
+		#endif
+			]], { i(0) })
+	),
+
+	s({
+		trig = '#err',
+		name = '#error',
+		desc = '#error snippet'
+	}, {
+		t('#error "'),
+		i(0, 'MSG'),
+		t('"')
+	}),
+
+	s({
+		trig = '#warn',
+		name = '#warning',
+		desc = '#warning snippet'
+	}, {
+		t('#warning "'),
+		i(0, 'MSG'),
+		t('"')
+	}),
+
+	s({
+			trig = 'if',
+			name = 'if',
+			desc = '"if" snippet',
+		},
+		fmta([[
+		if (<>) {
+		<>
+		}<>
+			]], {
+			i(1, 'true'),
+			i(2),
+			i(0)
+		})
+	),
+
+	s({
+			trig = 'else',
+			name = 'else',
+			desc = '"else" snippet',
+		},
+		fmta([[
+		<> {
+		<>
+		}<>
+			]], {
+			c(1, {
+				t('else'),
+				sn(1, {
+					t('else if ('),
+					i(1, 'true'),
+					t(')')
+				})
+			}),
+			i(2),
+			i(0)
+		})
+	),
+
+	s({
+			trig = 'switch',
+			name = 'switch',
+			desc = '"switch" snippet',
+		},
+		fmta([[
+		switch (<>) {<>
+		}
+			]], {
+			i(1, 'expression'),
+			i(0)
+		})
+	),
+
+	s({
+			trig = 'case',
+			name = 'case',
+			desc = '"case" branch'
+		},
+		fmta([[
+		case <>:<>
+			]], {
+			i(1, '0'),
+			i(0)
+		})
+	),
+
+	s({
+			trig = 'default',
+			name = 'default',
+			desc = '"default" branch'
+		},
+		t('default:')
+	),
+
+	s({
+			trig = 'while',
+			name = 'while',
+			desc = '"while" loop snippet'
+		},
+		fmta([[
+		while (<>) {<>
+		}
+			]], {
+			i(1, 'true'),
+			i(0)
+		})
+	),
+
+	s({
+			trig = 'do',
+			name = 'do ... while',
+			desc = '"do ... while" loop snippet'
+		},
+		fmta([[
+		do {<>
+		} while (<>);
+			]], {
+			i(0),
+			i(1, 'false'),
+		})
+	),
+
+	s({
+		trig = 'return',
+		name = 'return',
+		desc = '"return" snippet',
+	}, {
+		t('return '),
+		i(1, '0'),
+		t(';')
+	}),
+
+	s({
+		trig = 'exit',
+		name = 'exit',
+		desc = '"exit" snippet',
+	}, {
+		t('exit('),
+		i(1, 'EXIT_FAILURE'),
+		t(');')
+	}),
+
+	s({
+			trig = 'for',
+			name = 'for',
+			desc = '"for" loop snippets'
+		},
+
+		fmta([[
+		for (<>) {<>
+		}
+		]], {
+			c(1, {
+				sn(nil, fmta('<>;<>;<>', {
+					i(1),
+					i(2),
+					i(3)
+				})),
+				sn(nil, fmta('<> <> = <>; <> <> <>; <><>', {
+					i(1, 'size_t'),
+					i(2, 'i'),
+					i(3, '0'),
+					rep(2),
+					c(4, {
+						t('<'),
+						t('<='),
+						t('>'),
+						t('>=')
+					}),
+					i(5, 'count'),
+					m(4, '^<.*', '++', '--'),
+					rep(2),
+				})),
+				sn(nil, fmta('<> <> = <>; <> <> <>; <> <> <>', {
+					i(1, 'size_t'),
+					i(2, 'i'),
+					i(3, '0'),
+					rep(2),
+					c(4, {
+						t('<'),
+						t('<='),
+						t('>'),
+						t('>=')
+					}),
+					i(5, 'count'),
+					rep(2),
+					m(4, '^<.*', '+=', '-='),
+					i(6, '1'),
+				})),
+			}),
+			i(0),
+		})
+	),
+
+	s({
+			trig = 'args',
+			name = 'Argument loop',
+			desc = '"for" loop for cmdline arguments'
+		},
+		fmta([[
+		for (int <> = <>; <> << argc; ++<>) {<>
+		}
+			]], {
+			i(1, 'i'),
+			i(2, '1'),
+			rep(1),
+			rep(1),
+			i(0)
+		})
+	),
+
+	s({
+			trig = 'fun',
+			name = 'Function',
+			desc = 'Make a function declaration/definition/pointer'
+		},
+		c(1, {
+			sn(nil, fmt('{} {}({})', {
+				i(2, 'void'),
+				i(1, 'fun'),
+				i(3, 'void')
+			})),
+			sn(nil,
+				fmt([[
+					{doc}
+					{return_type} {name}({params})
+					]], {
+					return_type = i(2, 'void'),
+					name = i(1, 'fun'),
+					params = i(3, 'void'),
+					doc = d(4, function (args)
+							-- local splitArgs = {}
+							-- for str in string.gmatch(args, ',') do
+							-- 	str = string.gsub(str, "%s+", "")
+							-- 	table.insert(splitArgs, str)
+							-- end
+							--
+							-- local nodes = {
+							-- 	t({'/**', '* '}),
+							-- 	i(1, 'basic description'),
+							-- 	t({'', '*'})
+							-- }
+							--
+							-- for index, arg in ipairs(splitArgs) do
+							-- 	nodes.insert(t('* @param '..arg))
+							-- 	nodes.insert(index + 1)
+							-- 	nodes.insert(t({'', ''}))
+							-- end
+							--
+							-- nodes.insert(t('*/'))
+
+							-- return sn(nil, nodes)
+							return sn(nil, t(args))
+					end, {3}),
+				})
+			)
+		})
+	)
 }
